1f04556fd27f784a595673276e6ddafcee20784f
keisuke sato <corydoras121@gmail.com>
2022-09-04 23:18:03 +0900

    [update]base

--- src/js/main.js
+++ src/js/main.js
@@ -34,6 +34,39 @@ class Cone {
   }
 }
 
+class Dodecahedron {
+  constructor() {
+    this.geom = new THREE.DodecahedronGeometry(1.0);
+    this.rotationX = 0;
+    this.rotationY = 0;
+    this.rotationZ = radians(-180);
+  }
+}
+class Icosahedron {
+  constructor() {
+    this.geom = new THREE.IcosahedronGeometry(1.0);
+    this.rotationX = 0;
+    this.rotationY = 0;
+    this.rotationZ = radians(-180);
+  }
+}
+class Octahedron {
+  constructor() {
+    this.geom = new THREE.OctahedronGeometry(1.0);
+    this.rotationX = 0;
+    this.rotationY = 0;
+    this.rotationZ = radians(-180);
+  }
+}
+class Tetrahedron {
+  constructor() {
+    this.geom = new THREE.TetrahedronGeometry(1.0);
+    this.rotationX = 0;
+    this.rotationY = 0;
+    this.rotationZ = radians(-180);
+  }
+}
+
 function init(){
 
   // setup
@@ -41,11 +74,15 @@ function init(){
 
   meshes = [];
   const gutter = { size: 4 };
-  const grid = { cols: 7, rows: 7 };
+  const grid = { cols: 5, rows: 5 };
   mouse3D = new THREE.Vector2();
   geometries = [
-    new Box(),
-    new Cone()
+    // new Box(),
+    // new Cone(),
+    new Dodecahedron(),
+    new Icosahedron(),
+    new Octahedron(),
+    new Tetrahedron()
   ];
 
 
@@ -65,7 +102,7 @@ function init(){
 
   //カメラ
   camera = new THREE.PerspectiveCamera(20, window.innerWidth / window.innerHeight, 1);
-  camera.position.set(0, 100, 0);
+  camera.position.set(0, 120, 0);
   camera.rotation.x = radian(-45);
   scene.add(camera);
 
@@ -83,9 +120,61 @@ function init(){
   controls.enableDamping = true;
 
   //ライト
-  const light = new THREE.DirectionalLight(0xffffff, 1.5);
-  light.position.set(1, 1, 1);
-  scene.add(light);
+  const light01 = new THREE.DirectionalLight(0xffffff, 1.5);
+  light01.position.set(5, 50, 0);
+  light01.castShadow = true;
+  scene.add(light01);
+
+  // const helper = new THREE.DirectionalLightHelper( light01, 5 );
+  // scene.add( helper );
+
+  // const light02 = new THREE.SpotLight(0xffffff);
+  // light02.position.set(-3, 100, 0);
+  // light02.lookAt(0, 0, 0);
+  // scene.add(light02);
+
+  // const spotLightHelper = new THREE.SpotLightHelper( light02 );
+  // scene.add( spotLightHelper );
+
+  // const light03 = new THREE.RectAreaLight('#341212', 1, 2000, 2000);
+  // light03.position.set(5, 50, 50);
+  // light03.lookAt(0, 0, 0);
+  // scene.add(light03);
+
+  const light03 = new THREE.PointLight(0xFFFFFF, 2, 50, 1.0);
+  light03.position.set(-5, 6, 0);
+  scene.add(light03);
+
+  const pointLightHelper = new THREE.PointLightHelper( light03 );
+  scene.add( pointLightHelper );
+
+  const light04 = new THREE.PointLight(0xFFFFFF, 2, 50, 1.0);
+  light04.position.set(4, 10, 0);
+  scene.add(light04);
+
+  const pointLightHelper02 = new THREE.PointLightHelper( light04 );
+  scene.add( pointLightHelper02 );
+
+  const light05 = new THREE.PointLight(0xFFFFFF, 4, 100, 2.0);
+  light05.position.set(4, 3, 0);
+  scene.add(light05);
+
+  const pointLightHelper03 = new THREE.PointLightHelper( light05 );
+  scene.add( pointLightHelper03 );
+
+
+  // const envImage = 'https://picsum.photos/id/223/1024/1024';
+  const envImage = 'https://picsum.photos/id/106/1024/1024';
+  const envTexures = [
+    envImage, //right
+    envImage, //left
+    envImage, //up
+    envImage, //down
+    envImage, //front
+    envImage, //back
+  ];
+  const cubeTextureLoader = new THREE.CubeTextureLoader()
+  const textureCube = cubeTextureLoader.load( envTexures );
 
   //メッシュグループ
   const group = new THREE.Object3D();
@@ -111,10 +200,15 @@ function init(){
   }
 
   //メッシュ
-  // const geometry = new THREE.BoxGeometry(50, 50, 50);
-  const material = new THREE.MeshStandardMaterial({color: 0x444444});
-  // const mesh = new THREE.Mesh(geometry, material);
-  // scene.add(mesh);
+  const material = new THREE.MeshPhysicalMaterial({
+    color: 0xFFFFFF,
+    roughness: 0.1, //つや
+    transmission: 0.98, //透明度
+    thickness: 1.2, //屈折
+    envMap: textureCube, // 環境マップ設定
+    envMapIntensity: 1.5,
+  });
+
 
   for(let row = 0; row < grid.rows; row++){
     meshes[row] = [];
@@ -143,21 +237,32 @@ function init(){
   }
 
   //床
-  const floorGeometry = new THREE.PlaneGeometry(100, 100);
-  const floorMaterial = new THREE.ShadowMaterial({ opacity: .3 });
-  const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-  floor.position.y = 0;
-  floor.receiveShadow = true;
-  floor.rotateX(- Math.PI / 2);
-  scene.add(floor);
+  // const floorGeometry = new THREE.PlaneGeometry(100, 100);
+  // const floorMaterial = new THREE.ShadowMaterial({ opacity: .3 });
+  // const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+  // floor.position.y = 0;
+  // floor.receiveShadow = true;
+  // floor.rotateX(- Math.PI / 2);
+  // scene.add(floor);
+
+  const bgTexture = new THREE.TextureLoader().load("https://picsum.photos/id/106/1024/1024");
+  const bgGeometry = new THREE.PlaneGeometry(25, 25);
+  const bgMaterial = new THREE.MeshBasicMaterial({ map: bgTexture });
+  const bgMesh = new THREE.Mesh(bgGeometry, bgMaterial);
+  bgMesh.position.set(0, -1, 0);
+  bgMesh.rotateX(- Math.PI / 2);
+  bgMesh.receiveShadow = true;
+  scene.add(bgMesh);
 
 
+  let step = 0;
+  let rad;
 
   function animate(){
     //カメラの視点からマウス座標をマッピング
     raycaster.setFromCamera(mouse3D, camera);
     //マウス座標が床の形状と交差するかどうかを確認
-    const intersects = raycaster.intersectObjects([floor]);
+    const intersects = raycaster.intersectObjects([bgMesh]);
 
     if (intersects.length) {
       //交点の x 位置と z 位置を取得
@@ -199,7 +304,7 @@ function init(){
             z: scale,
           });
 
-          gsap.to(mesh.rotation, .7, {
+          gsap.to(mesh.rotation, 0.9, {
             ease: Back.easeOut.config(1.7),
             x: map(mesh.position.y, -1, 1, radians(45), mesh.initialRotation.x),
             z: map(mesh.position.y, -1, 1, radians(-90), mesh.initialRotation.z),
@@ -208,6 +313,24 @@ function init(){
         }
       }
     }
+
+    step += 0.8;
+    rad = radian(step);
+
+    // light01.position.x = 10 * Math.cos(rad * 0.1);
+    // light01.position.z = 10 * Math.sin(rad * 0.1);
+
+    light03.position.x = 15 * Math.cos(rad * 0.8);
+    light03.position.z = 15 * Math.sin(rad * 0.8);
+
+    light04.position.x = 12 * Math.cos(rad);
+    light04.position.z = 12 * Math.sin(rad);
+
+    light05.position.x = 15 * Math.sin(rad * 1.5);
+    light05.position.z = 15 * Math.cos(rad * 1.5);
+
+    // light04.position.x += Math.sin(10);
+
 
     //アニメーション処理
     // mesh.rotation.y += 0.01;
